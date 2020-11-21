5e6b9bb6cfcf8bd2eb30780f3d8d60853967df47
ZAC <punsalang.zachary@gmail.com>
2020-11-20 18:05:42 -0800

    Added Intro Page, home page with cards, removed Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import Home from './components/pages/Home';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Intro from './components/pages/Intro';
+import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/AboutUs';
 import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/about-us' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-        </Switch>
-      </Router>
-  ); }
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Intro} />
+        <Route path='/home' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/about-us' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
 
-function Services() {
-  return (
-    <div className='services'>
-      <Cards />
-    </div>
-    
-  );
+export default function Services() {
+  return <h1 className='services '>Sign Up</h1>;
 }
-
-export default Services;
