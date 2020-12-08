da2f0cb7c6415552064ed00fbc0074765296f90b
ZAC <punsalang.zachary@gmail.com>
2020-12-07 17:20:21 -0800

    Fixed Dive Team/Login Css Container

--- src/pages/valves/Valve3.js
+++ src/pages/valves/Valve3.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import '../../App.css'
+import CardItem from '../../components/CardItem';
+import '../../components/Cards.css';
 
-export default function Valve3() {
-    return <h1 className='valve3'>Valve3</h1>;
-}
+function Valve3() {
+    return (
+    <div className='cards'>
+        <h1>Dive Team!</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__items'>
+                    <CardItem
+                    src='images/coaching.png'
+                    text='Tell a mentor about your personal situation'
+                    label='Coaching'
+                    path='/valve1'
+                    />
+                    <CardItem
+                    src='images/mentoring.png'
+                    text='Talk to your mentor one on one'
+                    label='Mentoring'
+                    path='/valve2'
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+    );
+  }
+  
+  export default Valve3;
