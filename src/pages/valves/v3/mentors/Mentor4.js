5b0cc39a5557460cdf753abb3161ed78427021c0
ZAC <punsalang.zachary@gmail.com>
2020-12-12 17:50:51 -0800

    Calendar: mentor4 API/CalendarID changed

--- src/pages/valves/v3/mentors/Mentor4.js
+++ src/pages/valves/v3/mentors/Mentor4.js
@@ -2,7 +2,7 @@ import React from "react";
 import Calendar from "@ericz1803/react-google-calendar"
 
 const API_KEY = "AIzaSyCwdZPdGzaJVpzMGtfBSzxpUaaaUs3Ixgw";
-const CALENDAR_ID = "4rl33ith0vjp3i21u1tn6sil60@group.calendar.google.com";
+const CALENDAR_ID = "sbb14q3drudm00gennimo9673o@group.calendar.google.com";
 
 function Mentor4() {
     return (
