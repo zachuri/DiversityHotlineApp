dfe16ad911bd07e1a1bdb67ff60f579db0b87ec2
ZAC <punsalang.zachary@gmail.com>
2020-12-14 11:52:00 -0800

    Profile: added Josh description

--- src/pages/Team/Josh.js
+++ src/pages/Team/Josh.js
@@ -7,7 +7,15 @@ function Josh() {
                <img className="profile-img" src="images/josh.jpg" alt="josh"/>
                 <h1 className="profile-name">Joshua Ma</h1> 
                 <p>
-                    Founder of Diversity Hotline
+                Greetings!  My name is Joshua Ma and I am founder and champion of 
+                Diversity Hotline; an app and web based platform that strives to provide 
+                an inclusive and safe space for all conversations. I am currently a junior 
+                at UCI who is proudly double majoring in Chinese Studies and Political Science with a 
+                minor in Civic and Community Engagement. Calling Southern California home, I am proud to 
+                say that I too have experienced my list of unconscious biases that led me to feel insecucure 
+                about my appearance and identity. My hope and dream is that one day, 
+                Diversity Hotline will not only be a space to talk about such experiences but also have the 
+                tools to prevent to educate and prevent such negative experiences from ever occuring.
                 </p>
            </div>
         </div>
